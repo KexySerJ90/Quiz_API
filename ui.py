70bb37c03606dc59eb7b9d8cb034a800925ff1c1
KexySerJ90 <exserj9011@gmail.com>
2023-08-25 22:21:21 +0300

    Beta version 0.85

--- ui.py
+++ ui.py
@@ -19,19 +19,33 @@ class QuizInterface:
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
         ri = PhotoImage(file='images/true.png')
         wi = PhotoImage(file='images/false.png')
-        self.ok_button = Button(image=ri, highlightthickness=0, borderwidth=0)
+        self.ok_button = Button(image=ri, highlightthickness=0, borderwidth=0,command=self.good_q)
         self.ok_button.grid(column=0, row=2)
-        self.wrong_button = Button(image=wi, highlightthickness=0, borderwidth=0)
+        self.wrong_button = Button(image=wi, highlightthickness=0, borderwidth=0,command=self.bad_q)
         self.wrong_button.grid(column=1, row=2)
         self.get_next_quest()
         self.window.mainloop()
 
     def get_next_quest(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
-
+        self.canvas.config(bg='white')
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text,text="Game Over")
+            self.ok_button.config(state="disabled")
+            self.wrong_button.config(state="disabled")
     def good_q(self):
-        self.quiz.check_answer("True")
+        self.give_feedback(self.quiz.check_answer("True"))
 
-    def bad_q(selfs):
-        self.quiz.check_answer("False")
+    def bad_q(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self,is_right):
+        if is_right:
+            self.canvas.config(bg='green')
+        else:
+            self.canvas.config(bg='red')
+        self.window.after(1000, self.get_next_quest)
+
