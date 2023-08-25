6f62a4535b63742efc10b4a2a0926b35c4ad9a5d
KexySerJ90 <exserj9011@gmail.com>
2023-08-25 23:42:27 +0300

    Beta version 0.9, add best score

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,9 +1,21 @@
 import html
+
+
 class QuizBrain:
 
     def __init__(self, q_list):
         self.question_number = 0
         self.score = 0
+        self.best = 0
+        try:
+            with open('data.txt') as f:
+                self.best = int(f.read())
+        except (FileNotFoundError, ValueError):
+            with open('data.txt', mode='w') as f:
+                f.write('0')
+        else:
+            with open('data.txt') as f:
+                self.best = int(f.read())
         self.question_list = q_list
         self.current_question = None
 
@@ -13,13 +25,15 @@ class QuizBrain:
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        q_text=html.unescape(self.current_question.text)
+        q_text = html.unescape(self.current_question.text)
         return f"Q.{self.question_number}: {q_text}"
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
+            if self.best <= self.score:
+                self.best = self.score
             return True
         else:
             return False
--- ui.py
+++ ui.py
@@ -12,6 +12,8 @@ class QuizInterface:
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
         self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
         self.score_label.grid(column=1, row=0)
+        self.best_label= Label(text="Best: 0", fg="white", bg=THEME_COLOR)
+        self.best_label.grid(column=0,row=0)
         self.canvas = Canvas(width=300, height=250, bg='white')
         self.question_text = self.canvas.create_text(150, 125, width=280, text='Some questions',
                                                      font=('Ariel', 20, 'italic'),
@@ -28,14 +30,18 @@ class QuizInterface:
 
     def get_next_quest(self):
         self.canvas.config(bg='white')
+        self.score_label.config(text=f"Score: {self.quiz.score}")
+        self.best_label.config(text=f"Best: {self.quiz.best}")
         if self.quiz.still_has_questions():
-            self.score_label.config(text=f"Score: {self.quiz.score}")
+
             q_text = self.quiz.next_question()
             self.canvas.itemconfig(self.question_text, text=q_text)
         else:
             self.canvas.itemconfig(self.question_text,text="Game Over")
             self.ok_button.config(state="disabled")
             self.wrong_button.config(state="disabled")
+            with open('data.txt', 'w') as f:
+                f.write(f'{self.quiz.best}')
     def good_q(self):
         self.give_feedback(self.quiz.check_answer("True"))
 
